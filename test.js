23313e19592366a8443bd528175695bb327143c8
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-10-27 19:44:45 +0400

    FizzBuzz

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+// 1) Проверка на палиндром
+
 function isPalindrome(str) {
   // 1) Декларируем переменную cleanStr
   let cleanStr = "";
@@ -22,4 +24,24 @@ function isPalindrome(str) {
 
   // 5) Сравниваем результат
   return cleanStr === reversedStr;
+}
+
+// 2) FizzBuzz
+
+function fizzBuzz() {
+  for (let i = 1; i <= 100; i++) {
+    // 1) Если кратно 3 или 5 выводим "FizzBuzz".
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      // 2) Если кратно 3 выводим "Fizz".
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      // 3) Если кратно 5 выводим "Buzz"
+      console.log("Buzz");
+    } else {
+      // 4) Во всех остальных случаях выводим цифру
+      console.log(i);
+    }
+  }
 }
